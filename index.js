d10a37e9f630487fae1087cdedce88a0f9e06cf6
Kin <kin234142@gmail.com>
2026-03-03 17:47:14 +0500

    first

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+let todos = [];
+for (let file of files) {
+    file = file.split('\n');
+    for (let line of file) {
+        let ind = line.indexOf('// TODO');
+        if (ind !== -1) {
+            todos.push(line.slice(ind,));
+        }
+    }
+}
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -11,10 +21,17 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            getToDo();
+            break;
+        case 'important':
+            getImportant();
             break;
         default:
             console.log('wrong command');
@@ -22,4 +39,16 @@ function processCommand(command) {
     }
 }
 
-// TODO you can do it!
+function getToDo() {
+    for (let todo of todos) {
+            console.log(todo);
+        }
+}
+
+function getImportant(){
+    for (let todo of todos) {
+        if (todo.includes("!")) {
+            console.log(todo);
+        }
+    }
+}
